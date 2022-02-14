d20e785f722ea1e6af4af92fbb8f981869973073
mohamedibrahim1990 <43402445+mohamedibrahim1990@users.noreply.github.com>
2022-02-14 21:45:26 +0200

    Listing 1 and 2

---
+++ 1-Listing2-1/1-Listing2-1/1-Listing2-1.cpp
@@ -0,0 +1,27 @@
+// 1-Listing2-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    unsigned short a = 0b10101010;
+    printf("%hu\n", a);
+    int b = 0123;
+    printf("%d\n", b);
+    unsigned long long d = 0xFFFFFFFFFFFFFFFF;
+    printf("%llu\n", d);
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
