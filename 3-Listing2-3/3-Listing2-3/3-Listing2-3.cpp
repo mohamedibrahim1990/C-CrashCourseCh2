09b3628d46fa735fb0c81de1a832529e825bcf5c
mohamedibrahim1990 <mohamedibrahim51990@gmail.com>
2022-02-15 13:03:49 +0200

    Listing3

---
+++ 3-Listing2-3/3-Listing2-3/3-Listing2-3.cpp
@@ -0,0 +1,25 @@
+// 3-Listing2-3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    double an = 6.022140e23;
+    printf("Avogadros number: %le %lf %lg \n ", an, an, an);
+    float hp = 9.75;
+    printf("Hogwarts platform %e %f %g\n", hp, hp, hp);
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
